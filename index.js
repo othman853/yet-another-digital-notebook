db6e1ce559abe92fb1e221469b4ac56d891df630
othman853 <anuar.yasser@gmail.com>
2017-05-07 20:45:13 -0300

    Text editor prototype with html5

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', (_, res) => res.sendFile(__dirname + '/index.html'));
+
+app.listen(3000, console.log.bind(console, 'Up'));
